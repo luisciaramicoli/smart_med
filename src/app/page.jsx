113ea961c113c6ec25d9cea61f0a73a764f02d6d
luisciaramicoli <luisgustavobonfim996@gmail.com>
2024-08-14 09:03:22 -0300

    home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
-
+import { FaBriefcase, FaLightbulb, FaCog } from "react-icons/fa";
 export default function Home() {
   return (
     <div className="containerGlobal">
@@ -16,10 +16,51 @@ export default function Home() {
           </Link>
         </nav>
       </header>
-      <h1 className={styles.tituloHomePage}>Aula Next.js</h1>
+      <div className={styles.fundo}>
+      <div className={styles.quadroMaior}>
         <div className={styles.quadro}>
+        <h1 className={styles.tituloHomePage}>HealthWatch</h1>
+          <div className={styles.titulo}>
+            <h1>Informações</h1>
+          </div>
+          <div className={styles.topicos}>
+            <div className={styles.topico1}>
+              <div className={styles.circulo}>
+                < FaBriefcase className={styles.icone} />
+              </div>
+              <div className={styles.textoTopico1}>
+                BPMs
+              </div>
+            </div>
 
+            <div className={styles.topico2}>
+              <div className={styles.circulo}>
+                <FaLightbulb className={styles.icone} />
+              </div>
+              <div className={styles.textoTopico2}>
+                Oxigenio no sangue
+              </div>
+            </div>
+
+            <div className={styles.topico3}>
+              <div className={styles.circulo}>
+                <FaCog className={styles.icone} />
+              </div>
+              <div className={styles.textoTopico3}>
+                Sono
+              </div>
+            </div>
+          </div>
+          <div className={styles.texto}>
+            <p>
+              O HealthWatch transmite as informações recebidas pelo smartWatch utilizado pelo paciente,
+              lembre-se que a total responsabilidade pelo uso durante os dias são por parte do mesmo, o médiuco só será responsavel
+              pela análise dos dados aqui transmitidos.
+            </p>
+          </div>
         </div>
+      </div>
+      </div>
     </div>
   );
 }
